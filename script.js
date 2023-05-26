21467ddae8888b20ca472eabe848aed788717868
Chinwe Onumonu <kaforglo122@yahoo.com>
2023-05-26 18:12:56 +0100

    add cross icon for deleting

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML
     }
     inputBox.value = "";
 }
