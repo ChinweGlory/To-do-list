de9fcbde11e20390d61df507c114dc0610968672
Chinwe Onumonu <kaforglo122@yahoo.com>
2023-05-26 13:18:40 +0100

    define the onclick on the button in the html

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // create a variable
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+
+// call onclick in a funstion
+function addTask() {
+    if(inputBox.value === '') {
+        alert ("write something");
+    }
+}
